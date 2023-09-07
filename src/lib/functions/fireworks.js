6f7c5981b6c4c4f3e32b2ffb8542552ea8d8b5e4
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-07 15:45:54 -0500

    ran formatter.

--- src/lib/functions/fireworks.js
+++ src/lib/functions/fireworks.js
@@ -9,7 +9,6 @@ const chance = new Chance()
 // credit is due to this blocks page for the process defined below: http://bl.ocks.org/s2t2/53e96654487b4b0ef6e5
 // I took what was there, made adjustments on preference/version differences, and added to it.
 export async function launchFireworkBurst() {
-
   let width = d3.select("#fireworks").node().clientWidth
   let height = d3.select("#fireworks").node().clientHeight
   // defining y parameter for the height of the launch
