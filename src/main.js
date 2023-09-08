1a262a6f4114de03dd9ff06fbc5daf581a8b9287
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-08 16:32:02 -0500

    fixed classlist add.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import "../node_modules/svelte-lib/src/lib/static/styles/root.css"
 import "../node_modules/svelte-lib/src/lib/static/styles/app.css"
 
 let div = document.createElement("div")
-div.classList.add("fireworks fixed top-0 left-0 w-full h-full z-50")
+div.classList.add("fireworks")
 
 let script = document.currentScript
 script.parentNode.insertBefore(div, script)
