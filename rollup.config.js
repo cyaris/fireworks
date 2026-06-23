e7c56ec8742a5e161ab3148624cb84be6accf298
Charlie Yaris <charlieyaris@Charlies-MacBook-Pro.local>
2026-06-23 19:49:19 +0100

    refactored rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,21 @@
-import commonjs from "@rollup/plugin-commonjs"
-import image from "@rollup/plugin-image"
-import resolve from "@rollup/plugin-node-resolve"
-import json from "@rollup/plugin-json"
-import url from "@rollup/plugin-url"
-import postcss from "rollup-plugin-postcss"
-import progress from "rollup-plugin-progress"
-import svelte from "rollup-plugin-svelte"
-import svg from "rollup-plugin-svg"
-import tailwindcss from "tailwindcss"
+import { createRollupConfig } from "svelte-lib/rollup.config.js"
 
-import tailwindConfig from "./tailwind.config.cjs"
-import addScopedClass from "./add-scoped-class-plugin.cjs"
-
-const plugins = [
-  resolve({
-    browser: true,
-    dedupe: ["svelte"],
-    exportConditions: ["svelte"],
-    extensions: [".svelte"],
-  }),
-  json(),
-  commonjs(),
-  svelte({
-    emitCss: true,
-  }),
-  svg(),
-
-  image(),
-  url({
-    include: ["**/*.gif"],
-    limit: 0, // set to 0 to embed all .gif files as base64 data URLs
-    emitFiles: false,
-  }),
-  postcss({
-    plugins: [addScopedClass(), tailwindcss(tailwindConfig)],
-    extract: true,
-    minimize: true,
-    config: {
-      path: "./postcss.config.cjs",
+export default createRollupConfig({
+  scopeClass: "fireworks",
+  entries: [
+    {
+      input: "./src/main.js",
+      output: {
+        format: "iife",
+        file: "dist/bundle.js",
+      },
     },
-    extensions: [".css"],
-    // inject: {
-    //   insertAt: "top",
-    // },
-    sourceMap: false,
-  }),
-  progress(),
-]
-
-const config = [
-  {
-    input: "./src/main.js",
-    output: {
-      format: "iife",
-      file: "dist/bundle.js",
+    {
+      input: "./src/main2.js",
+      output: {
+        format: "es",
+        file: "dist/bundle2.js",
+      },
     },
-    plugins: plugins,
-  },
-  {
-    input: "./src/main2.js",
-    output: {
-      format: "es",
-      file: "dist/bundle2.js",
-    },
-    plugins: plugins,
-  },
-]
-
-console.log(config)
-export default config
+  ],
+})
