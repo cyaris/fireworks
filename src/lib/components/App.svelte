6e370576af28e001f1a4a3cf98a3e7dc0c4eea51
Charlie Yaris <35279417+cyaris@users.noreply.github.com>
2026-09-03 10:28:33 -0500

    Use shared zoom-stable resize observation

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,26 +1,17 @@
 <script>
   import { onMount } from "svelte"
-  import { createZoomStableViewport } from "svelte-lib/functions"
+  import { observeZoomStableViewport } from "svelte-lib/functions"
 
   let width
   let height
-  let zoomStableViewport = createZoomStableViewport()
 
-  function syncViewportSize() {
-    let viewport = zoomStableViewport.update()
-
+  function syncViewportSize(viewport) {
     width = viewport.width
     height = viewport.height
   }
 
-  onMount(() => {
-    syncViewportSize()
-    window.visualViewport?.addEventListener("resize", syncViewportSize)
-
-    return () => window.visualViewport?.removeEventListener("resize", syncViewportSize)
-  })
+  onMount(() => observeZoomStableViewport(syncViewportSize))
 </script>
 
-<svelte:window on:resize={syncViewportSize} />
 <canvas class="non-reactive fixed left-0 top-0 z-[100]" id="fireworks" style:width="{width}px" style:height="{height}px"
 ></canvas>
