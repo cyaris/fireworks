ec43c2302368cdc27c1e9aec4b7c9263012bb46b
Charlie Yaris <35279417+cyaris@users.noreply.github.com>
2026-09-03 09:47:51 -0500

    Keep the fireworks canvas stable during zoom

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,8 +1,26 @@
 <script>
+  import { onMount } from "svelte"
+  import { createZoomStableViewport } from "svelte-lib/functions"
+
   let width
   let height
+  let zoomStableViewport = createZoomStableViewport()
+
+  function syncViewportSize() {
+    let viewport = zoomStableViewport.update()
+
+    width = viewport.width
+    height = viewport.height
+  }
+
+  onMount(() => {
+    syncViewportSize()
+    window.visualViewport?.addEventListener("resize", syncViewportSize)
+
+    return () => window.visualViewport?.removeEventListener("resize", syncViewportSize)
+  })
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window on:resize={syncViewportSize} />
 <canvas class="non-reactive fixed left-0 top-0 z-[100]" id="fireworks" style:width="{width}px" style:height="{height}px"
 ></canvas>
